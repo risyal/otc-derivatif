480f7e2594e1af36742da134a7c4ecced3b98e7f
risyal <risal.syah@gmail.com>
2020-06-24 15:15:25 +0700

    Updates

---
+++ precache-manifest.e94a6d1dabbe21474a1965a0db280fc9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2e45668375092b5be7d4b778287b619",
+    "url": "/otc-derivatif-project/index.html"
+  },
+  {
+    "revision": "8c4c0cc9347ec34a78ff",
+    "url": "/otc-derivatif-project/static/css/main.17174352.chunk.css"
+  },
+  {
+    "revision": "693b91734054ec41266c",
+    "url": "/otc-derivatif-project/static/js/2.7285d021.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/otc-derivatif-project/static/js/2.7285d021.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c4c0cc9347ec34a78ff",
+    "url": "/otc-derivatif-project/static/js/main.33be7b48.chunk.js"
+  },
+  {
+    "revision": "8a87e3cb2bc6fd524421",
+    "url": "/otc-derivatif-project/static/js/runtime-main.95ff7a36.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/otc-derivatif-project/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "26d6e5e88d42309accaacb072eed3872",
+    "url": "/otc-derivatif-project/static/media/site-logo.26d6e5e8.png"
+  }
+]);
