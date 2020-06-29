05803de68865b4b247a8e5f0d5a1e1be0ee314a1
risyal <risal.syah@gmail.com>
2020-06-29 17:30:10 +0700

    inquiry position update

--- src/component/views/clearing/InquiryPosition.js
+++ src/component/views/clearing/InquiryPosition.js
@@ -1,5 +1,11 @@
-import React from 'react';
-import { Table } from 'antd';
+import React, { useState } from 'react'
+import {
+    Form,
+    Button,
+    Input,
+    Table,
+    Select,
+} from 'antd';
 function InquiryPosition() {
     const columns = [
         {
@@ -121,8 +127,56 @@ function InquiryPosition() {
         {
         },
     ];
+    const componentSize = 'middle';
+    const formItemLayout = {
+        labelCol: {
+            xs: { span: 24 },
+            sm: { span: 6 },
+        },
+        wrapperCol: {
+            xs: { span: 24 },
+            sm: { span: 16 },
+        },
+    };
+    const productSelect = ['OIS', 'IRS', 'DNDF'];
+    const [jenisProduct, SetJenisProduct] = useState(productSelect[0]);
+    const productClick = (e) => {
+        SetJenisProduct(e);
+    };
     return (
         <div style={{ margin: '15px 20px' }} scroll={{ x: 1300 }}>
+            <Form
+                {...formItemLayout}
+                size={componentSize}
+                layout="horizontal"
+                initialValues={{ size: componentSize }}
+                labelAlign="left"
+            >
+                <Form.Item label="SID" >
+                    <Input />
+                </Form.Item>
+                <Form.Item label="Product ">
+                    <Select defaultValue={jenisProduct} onChange={productClick}>
+                        {productSelect.map(product => (
+                            <Select.Option key={product}>{product}</Select.Option>
+                        ))}
+                    </Select>
+                </Form.Item>
+                <Form.Item label="UTI" >
+                    <Input />
+                </Form.Item>
+                <Form.Item label="Member" >
+                    <Input />
+                </Form.Item>
+                <Form.Item label="Reference Number" >
+                    <Input />
+                </Form.Item>
+                <Form.Item wrapperCol={{ span: 12, offset: 6 }}>
+                    <Button type="primary" htmlType="submit">
+                        Search
+                    </Button>
+                </Form.Item>
+            </Form>
             <Table
                 columns={columns}
                 dataSource={data}
