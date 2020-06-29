d931b19f9c85e1722e2f9151713e3481a07a377e
ririn_fa <ririn.nfa@gmail.com>
2020-06-29 14:41:33 +0700

    Font header-content

--- src/component/config/RoutePage.js
+++ src/component/config/RoutePage.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Typography } from 'antd';
 import LoginPage from '../views/Login';
 import NewTradePage from '../views/trade/NewTrade';
 import InquiryTradePage from '../views/trade/InquiryTrade';
@@ -52,155 +53,157 @@ import InstructionCOLDP from '../views/collateral/InstructionCOLDP';
 import InstructionCOLW from '../views/collateral/InstructionCOLW';
 
 function RoutePage() {
+    const { Title } = Typography;
+    
     return (
         <div>
             <Route path="/inquerytrade">
                 <div className="head-content">
-                    Inquiry Input Trade
+                    <Title level={4}>Inquiry Input Trade</Title>
                 </div>
                 <InquiryTradePage />
             </Route>
             <Route path="/newtrade">
                 <div className="head-content">
-                    Input Trade
+                    <Title level={4}>Input Trade</Title>
                 </div>
                 <NewTradePage />
             </Route>
             <Route path="/canceltrade">
                 <div className="head-content">
-                    Cancel Trade Input Trade
+                    <Title level={4}>Cancel Trade Input Trade</Title>
                 </div>
                 <CancelTradePage />
             </Route>
             <Route path="/monitoringtrade">
                 <div className="head-content">
-                    Monitoring Trade
+                    <Title level={4}>Monitoring Trade</Title>
                 </div>
                 <MonitoringTrade />
             </Route>
             <Route path="/tradesummary">
                 <div className="head-content">
-                    Trade summary
+                    <Title level={4}>Trade summary</Title>
                 </div>
                 <TradeSummary />
             </Route>
             <Route path="/clearingManagement/clearingPosition">
                 <div className="head-content">
-                    Clearing Position
+                    <Title level={4}>Clearing Position</Title>
                 </div>
                 <ClearingPostition />
             </Route>
             <Route path="/clearingManagement/inquiryPostition">
                 <div className="head-content">
-                    Inquiry Position
+                    <Title level={4}>Inquiry Position</Title>
                 </div>
                 <InquiryPosition />
             </Route>
             <Route path="/clearingManagement/settlementPostion">
                 <div className="head-content">
-                    Settlement Position
+                    <Title level={4}>Settlement Position</Title>
                 </div>
                 <SettlementPosition />
             </Route>
             <Route path="/clearingManagement/cashFlowIrs">
                 <div className="head-content">
-                    Cash Flow IRS Calculation
+                    <Title level={4}>Cash Flow IRS Calculation</Title>
                 </div>
                 <CashFlowIrs />
             </Route>
             <Route path="/accountManagement/detailinformation">
                 <div className="head-content">
-                    Account Detail Information
+                    <Title level={4}>Account Detail Information</Title>
                 </div>
                 <AccountDetail />
             </Route>
             <Route path="/accountManagement/collateralMember">
                 <div className="head-content">
-                    Detail Blocked Collateral - Member
+                    <Title level={4}>Detail Blocked Collateral - Member</Title>
                 </div>
                 <BlockCollateralMember />
             </Route>
             <Route path="/accountManagement/collateralClient">
                 <div className="head-content">
-                    Detail Blocked Collateral - Client
+                    <Title level={4}>Detail Blocked Collateral - Client</Title>
                 </div>
                 <BlockCollateralClient />
             </Route>
             <Route path="/accountManagement/detailFund">
                 <div className="head-content">
-                    Detail Default Fund
+                    <Title level={4}>Detail Default Fund</Title>
                 </div>
                 <DefaultFund />
             </Route>
             <Route path="/accountManagement/registerBeneficiaries">
                 <div className="head-content">
-                    Register Beneficiaries
+                    <Title level={4}>Register Beneficiaries</Title>
                 </div>
                 <RegisterBeneficiaries />
             </Route>
             <Route path="/accountManagement/inquiryBeneficiaries">
                 <div className="head-content">
-                    Inquiry Beneficiaries
+                    <Title level={4}>Inquiry Beneficiaries</Title>
                 </div>
                 <InquiryBeneficiaries />
             </Route>
             <Route path="/settlement/inquirySettlement">
                 <div className="head-content">
-                    Inquiry Settlement Instruction
+                    <Title level={4}>Inquiry Settlement Instruction</Title>
                 </div>
                 <SettlementInstruction />
             </Route>
             <Route path="/settlement/settlementExecution">
                 <div className="head-content">
-                    Settlement Job Execution
+                    <Title level={4}>Settlement Job Execution</Title>
                 </div>
                 <SettlementJobExecution />
             </Route>
             <Route path="/settlement/monitoringClearing">
                 <div className="head-content">
-                    Monitoring Clearing Pos vs Balance
+                    <Title level={4}>Monitoring Clearing Pos vs Balance</Title>
                 </div>
                 <SettlementMonitoring />
             </Route>
             <Route path="/instructionManagement/securityManagement">
                 <div className="head-content">
-                    Security Management (Instruction, Request for cancel)
+                    <Title level={4}>Security Management (Instruction, Request for cancel)</Title>
                 </div>
                 <SecurityManagement />
             </Route>
             <Route path="/instructionManagement/inquirySecManagement">
                 <div className="head-content">
-                    Inquiry Sec Management
+                    <Title level={4}>Inquiry Sec Management</Title>
                 </div>
                 <InquirySecManagement />
             </Route>
             <Route path="/instructionManagement/cashManagement">
                 <div className="head-content">
-                    Cash Management (Instruction, Request for cancel)
+                    <Title level={4}>Cash Management (Instruction, Request for cancel)</Title>
                 </div>
                 <CashInquiryManagement />
             </Route>
             <Route path="/instructionManagement/inquiryCashManagement">
                 <div className="head-content">
-                    Inquiry Cash Management
+                    <Title level={4}>Inquiry Cash Management</Title>
                 </div>
                 <InquiryCashManagement />
             </Route>
             <Route path="/collateralManagement/inquiry">
                 <div className="head-content">
-                    Inquiry
+                    <Title level={4}>Inquiry</Title>
                 </div>
                 <Inquiry />
             </Route>
             <Route path="/collateralManagement/instructionColdp">
                 <div className="head-content">
-                    Instruction - COLDP (Create, Check, Accept, Request for cancel)
+                    <Title level={4}>Instruction - COLDP (Create, Check, Accept, Request for cancel)</Title>
                 </div>
                 <InstructionCOLDP />
             </Route>
             <Route path="/collateralManagement/instructionColw">
                 <div className="head-content">
-                    Instruction - COLW (Create, Check, Accept, Request for cancel)
+                    <Title level={4}>Instruction - COLW (Create, Check, Accept, Request for cancel)</Title>
                 </div>
                 <InstructionCOLW />
             </Route>
@@ -212,133 +215,133 @@ function RoutePage() {
             </Route>
             <Route path="/obligationaccomplishment">
                 <div className="head-content">
-                    Obligation Accomplishment
+                    <Title level={4}>Obligation Accomplishment</Title>
                 </div>
                 <ObligationAccomplishment />
             </Route>
             <Route path="/dailytransaction">
                 <div className="head-content">
-                    Daily Transaction Report
+                    <Title level={4}>Daily Transaction Report</Title>
                 </div>
                 <DailyTransactionReport />
             </Route>
             <Route path="/novationreport">
                 <div className="head-content">
-                    Novation Report
+                    <Title level={4}>Novation Report</Title>
                 </div>
                 <NovationReport />
             </Route>
             <Route path="/movementbalance">
                 <div className="head-content">
-                    Movement Balance
+                    <Title level={4}>Movement Balance</Title>
                 </div>
                 <MovementBalance />
             </Route>
             <Route path="/fundreport">
                 <div className="head-content">
-                    Default Fund Report
+                    <Title level={4}>Default Fund Report</Title>
                 </div>
                 <DefaultFundReport />
             </Route>
             <Route path="/reportbankid">
                 <div className="head-content">
-                    <h1>Header Content</h1>
+                    <Title level={4}>Report Bank Indonesia</Title>
                 </div>
                 <ReportBI />
             </Route>
             <Route path="/feereport">
                 <div className="head-content">
-                    Fee Report
+                    <Title level={4}>Fee Report</Title>
                 </div>
                 <FeeReport />
             </Route>
             <Route path="/dhkmemberclient">
                 <div className="head-content">
-                    DHK level member dan client
+                    <Title level={4}>DHK level member dan client</Title>
                 </div>
                 <DHKLevel />
             </Route>
             <Route path="/registerclient">
                 <div className="head-content">
-                    Register Client
+                    <Title level={4}>Register Client</Title>
                 </div>
                 <RegisterClient />
             </Route>
             <Route path="/registermember">
                 <div className="head-content">
-                    Register Member
+                    <Title level={4}>Register Member</Title>
                 </div>
                 <RegisterMember />
             </Route>
             <Route path="/registercontract">
                 <div className="head-content">
-                    Register Contract (KPEI)
+                    <Title level={4}>Register Contract (KPEI)</Title>
                 </div>
                 <RegisterContract />
             </Route>
             <Route path="/editaccount">
                 <div className="head-content">
-                    Edit Account (Status)
+                    <Title level={4}>Edit Account (Status)</Title>
                 </div>
                 <EditAccount />
             </Route>
             <Route path="/securitiescollmgt">
                 <div className="head-content">
-                    Securities Coll Management (Register Inst, Eligibility, Haircut)
+                    <Title level={4}>Securities Coll Management (Register Inst, Eligibility, Haircut)</Title>
                 </div>
                 <SecuritiesCollMgt />
             </Route>
             <Route path="/cashcollmgt">
                 <div className="head-content">
-                    Cash Coll Management (Register Mata Uang, Eligibility, Haircut)
+                    <Title level={4}>Cash Coll Management (Register Mata Uang, Eligibility, Haircut)</Title>
                 </div>
                 <CashCollMgt />
             </Route>
             <Route path="/editreferencerate">
                 <div className="head-content">
-                    Edit Reference Rate (JIBOR, JISDOR, INDONIA, LIBOR)
+                    <Title level={4}>Edit Reference Rate (JIBOR, JISDOR, INDONIA, LIBOR)</Title>
                 </div>
                 <EditReferenceRate />
             </Route>
             <Route path="/editparameter">
                 <div className="head-content">
-                    Edit Parameter
+                    <Title level={4}>Edit Parameter</Title>
                 </div>
                 <EditParameter />
             </Route>
             <Route path="/audittrail">
                 <div className="head-content">
-                    Audit trail
+                    <Title level={4}>Audit trail</Title>
                 </div>
                 <AuditTrail />
             </Route>
             <Route path="/usermanagement">
                 <div className="head-content">
-                    User Management
+                    <Title level={4}>User Management</Title>
                 </div>
                 <UserManagement />
             </Route>
             <Route path="/systemparameter">
                 <div className="head-content">
-                    System Parameter
+                    <Title level={4}>System Parameter</Title>
                 </div>
                 <SystemParameter />
             </Route>
             <Route path="/calendar">
                 <div className="head-content">
-                    Calender (Hari Libur/BI Calender)
+                    <Title level={4}>Calender (Hari Libur/BI Calender)</Title>
                 </div>
                 <Calendar />
             </Route>
             <Route path="/approval">
                 <div className="head-content">
-                    Approval
+                    <Title level={4}>Approval</Title>
                 </div>
                 <Approval />
             </Route>
             <Route path="/registerats">
                 <div className="head-content">
-                    Register ATS
+                    <Title level={4}>Register ATS</Title>
                 </div>
                 <RegisterAts />
             </Route>
@@ -346,4 +349,5 @@ function RoutePage() {
         </div>
     )
 }
+
 export default RoutePage;
