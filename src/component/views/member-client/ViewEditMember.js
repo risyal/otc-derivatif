cf8890298583ea78ac727aeb74b1b75bdade66eb
risyal <risal.syah@gmail.com>
2020-07-01 20:39:00 +0700

    update view edit

--- src/component/views/member-client/ViewEditMember.js
+++ src/component/views/member-client/ViewEditMember.js
@@ -3,6 +3,7 @@ import {
     Form,
     Input,
     Button,
+    Select,
 } from 'antd';
 import { Typography } from 'antd';
 import { Link } from "react-router-dom";
@@ -25,40 +26,68 @@ const ViewEditMember = (props) => {
         {
             key: '0',
             memberID: ' ',
+            sidLei: ' ',
             namaPerusahaan: ' ',
             alamat: ' ',
             pic: ' ',
             noTelp: ' ',
             email: ' ',
+            bic: ' ',
+            collateral: ' ',
+            settlement: ' ',
+            dFund: ' ',
+            ssss: ' ',
             status: ' ',
-        }, {
+        },
+        {
             key: '1',
+            no: '1',
             memberID: 'Member123',
+            sidLei: 'ID12',
             namaPerusahaan: 'PT Jaya Abadi',
             alamat: 'New York No. 1 Lake Park',
             pic: 'John Brown',
             noTelp: '085112345227',
             email: 'john@gmail.com',
+            bic: 'BIC01',
+            collateral: 'Collateral1',
+            settlement: 'Settlement1',
+            dFund: 'Def-Fund1',
+            ssss: 'SSSS1',
             status: 'status1',
         },
         {
             key: '2',
+            no: '2',
             memberID: 'Member345',
+            sidLei: 'ID23',
             namaPerusahaan: 'PT Citra Utama',
             alamat: 'New York No. 1 Lake Park',
             pic: 'Jim Green',
             noTelp: '085112345227',
             email: 'jim@gmail.com',
+            bic: 'BIC02',
+            collateral: 'Collateral2',
+            settlement: 'Settlement2',
+            dFund: 'Def-Fund2',
+            ssss: 'SSSS2',
             status: 'status3',
         },
         {
             key: '3',
+            no: '3',
             memberID: 'Member567',
+            sidLei: 'ID34',
             namaPerusahaan: 'PT Abadi Makmur',
             alamat: 'New York No. 1 Lake Park',
             pic: 'John Black',
             noTelp: '085112345227',
             email: 'black@gmail.com',
+            bic: 'BIC03',
+            collateral: 'Collateral3',
+            settlement: 'Settlement3',
+            dFund: 'Def-Fund3',
+            ssss: 'SSSS3',
             status: 'status2',
         },
     ];
@@ -83,6 +112,9 @@ const ViewEditMember = (props) => {
             > <Form.Item label="Member ID">
                     <Input disabled={disable} value={dataMemberById.memberID} />
                 </Form.Item>
+                <Form.Item label="SID/LEI">
+                    <Input disabled={disable} value={dataMemberById.sidLei} />
+                </Form.Item>
                 <Form.Item label="Nama Perusahaan">
                     <Input disabled={disable} value={dataMemberById.namaPerusahaan} />
                 </Form.Item>
@@ -97,6 +129,27 @@ const ViewEditMember = (props) => {
                 </Form.Item>
                 <Form.Item label="Email">
                     <Input disabled={disable} value={dataMemberById.email} />
+                </Form.Item>
+                <Form.Item label="Kode BIC">
+                    <Input disabled={disable} value={dataMemberById.bic} />
+                </Form.Item>
+                <Form.Item label="RTGS Account">
+                    <Input
+                        addonBefore="Collateral"
+                        value={dataMemberById.collateral}
+                        disabled={disable} />
+                    <Input
+                        addonBefore="Settlement"
+                        value={dataMemberById.settlement}
+                        disabled={disable} />
+                    <Input
+                        addonBefore="Default Fund"
+                        value={dataMemberById.dFund}
+                        disabled={disable}
+                    />
+                </Form.Item>
+                <Form.Item label="SSSS Account">
+                    <Input disabled={disable} value={dataMemberById.ssss} />
                 </Form.Item>
                 <Form.Item label="Status">
                     <Input disabled={disable} value={dataMemberById.status} />
